80df5cc6e14057b1dbda17f47dbd27012b81dfdd
Artash62062 <srabyanart@gmail.com>
2021-04-29 11:54:18 +0400

    Adding new task solutions

--- src/Day4/AndOr.java
+++ src/Day4/AndOr.java
@@ -13,11 +13,11 @@ public class AndOr {
         System.out.print("Input the num:");
         int inputNum = scanner.nextInt();
 
-        if(inputNum>10&& inputNum<=100) {
+        if (inputNum > 10 && inputNum <= 100) {
             System.out.println("input Number is in range of 10->100");
-        } else if ( inputNum>100 && inputNum<=200 ) {
+        } else if (inputNum > 100 && inputNum <= 200) {
             System.out.println("Input num is in range of 100->200");
-        } else if ( inputNum > 200 && inputNum <=300) {
+        } else if (inputNum > 200 && inputNum <= 300) {
             System.out.println("Input num is in range 200->300");
         } else {
             System.out.println("Number higher than 300 or number smaller than 10");
